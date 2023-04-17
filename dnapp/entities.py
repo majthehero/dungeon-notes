c716e81e5d088c0831347368b5c694e429b113f5
maj.smerkol+a@gmail.com <maj.smerkol@ijs.si>
2023-04-17 23:58:45 +0200

    meh

--- dnapp/entities.py
+++ dnapp/entities.py
@@ -6,13 +6,13 @@ db = Database()
 
 class Note(db.Entity):
     id = PrimaryKey(int, auto=True)
-    text = Required(str)
+    author = Required("User")
+    campaign = Required("Campaign")
+    tags = Set("Tag")
     location = Optional("Location")
     date = Required(str)
     time = Required(str)
-    tags = Set("Tag")
-    campaign = Required("Campaign")
-    author = Required("User")
+    text = Required(str)
 
 
 class Location(db.Entity):
@@ -26,24 +26,35 @@ class Location(db.Entity):
 
 class Tag(db.Entity):
     id = PrimaryKey(int, auto=True)
-    text = Required(str)
     notes = Set(Note)
     locations = Set(Location)
+    text = Required(str)
 
 
 class Campaign(db.Entity):
     id = PrimaryKey(int, auto=True)
-    title = Required(str)
-    notes = Set(Note)
     dm = Required("User", reverse="masters_campaigns")
     players = Set("User", reverse="plays_campaigns")
+    nicknames = Set("Nickname", reverse="campaign")
+    notes = Set(Note)
     locations = Set(Location)
+
+
+class Nickname(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    campaign = Required(Campaign)
+    user = Required("User")
+    text = Required(str)
 
 
 class User(db.Entity, UserMixin):
     id = PrimaryKey(int, auto=True)
     email = Required(str)
     password = Required(str)
+
     masters_campaigns = Set(Campaign, reverse="dm")
     plays_campaigns = Set(Campaign, reverse="players")
     authored_notes = Set(Note)
+    nicknames = Set(Nickname, reverse="user")
+    opt_ui_dark = Optional(int, size=8, default=0)
+    opt_default_nick = Optional(str)
--- dnapp/templates/template_strings.py
+++ dnapp/templates/template_strings.py
@@ -1,16 +1,20 @@
 # template strings
 # campaign
 campaign_list_item = """
-<h3>
-  <a href="{{ url_for('api.timeline', id=campaign.id) }}">
-    {{campaign.title}}
-  </a>
-  <span 
+<div>
+  <h3>
+    <a href="{{ url_for('api.timeline', id=campaign.id) }}">
+      {{campaign.title}}
+    </a>
+  </h3>
+  <span class="campaign-delete">
+    <a 
       hx-delete="{{ url_for('api.campaign', id=campaign.id) }}" 
-      hx-swap="delete" >
+      hx-swap="delete">
     Delete
-  </span>"
-</h3>
+    </a>
+  </span>
+</div>
 """
 campaign_list_item_deleted = """
 <p>deleted</p>"
@@ -19,11 +23,9 @@ campaign_list_item_deleted = """
 # timeline
 timeline_note = """
 <div class="timeline-note">
-  <p>
-    <span>{{note.time}}:</span>
-    <span>{{note.text}}</span>
-  </p>
-  <p>
+  <span class="note-time">{{note.time}}:</span>
+  <span class="note-text">{{note.text}}</span>
+  <p class="author">
     {{note.author.email}}
   </p>
 </div>
